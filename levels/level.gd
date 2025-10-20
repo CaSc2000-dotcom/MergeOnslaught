9ca5cbb83643b52040551fbe36867cd5b36a1fc1
CaSc2000-dotcom <39818295+CaSc2000-dotcom@users.noreply.github.com>
2025-10-20 17:04:31 -0600

    updated level.gd
    - game_over() and new_game()
    - timers connected
    - hit signal connected

--- levels/level.gd
+++ levels/level.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+
 @export var mob_scene: PackedScene # Drag your mob scene here in the Inspector
+var score: int
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	new_game()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,11 +15,33 @@ func _process(delta: float) -> void:
 	pass
 
 
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+
+func new_game() -> void:
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+
 func _on_mob_timer_timeout() -> void:
+	# Create a mob instance and add it to the scene
+	var mob: Node = mob_scene.instantiate()
 	# Choose a random location on the path
 	var mob_spawn_location: Node = $Player/Camera2D/SpawnPath/SpawnLocation
 	mob_spawn_location.progress_ratio = randf() # randf() gives a random float between 0 and 1
-	# Create a mob instance and add it to the scene
-	var mob: Node = mob_scene.instantiate()
+	# Set position to the random location
 	mob.global_position = mob_spawn_location.global_position
+	# Spawn the mob
 	add_child(mob)
+
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+
+
+func _on_start_timer_timeout() -> void:
+	$MobTimer.start()
+	$ScoreTimer.start()
